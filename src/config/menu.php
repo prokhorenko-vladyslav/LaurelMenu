440096da7fe47ad26b335fc45cac3b0e473d0f44
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-05-27 14:48:19 +0300

    Added user relations to config

--- src/config/menu.php
+++ src/config/menu.php
@@ -1,4 +1,12 @@
 <?php
     return [
+        /**
+         * Need to attach user to menu and menu item
+         */
+        'attach_user' => true,
 
+        /**
+         * User model
+         */
+        'user_model' => config('auth.providers.users.model'),
     ];
