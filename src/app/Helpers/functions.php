c284f1137a416a90bd1575d8cf87449d88f49ca7
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-05-27 17:52:09 +0300

    Added helpers

--- src/app/Helpers/functions.php
+++ src/app/Helpers/functions.php
@@ -1,1 +1,13 @@
 <?php
+    if (!function_exists('loadMenuBySlug')) {
+        function loadMenuBySlug(string $slug) {
+            \Laurel\Menu\MenuFacade::loadBySlug($slug);
+        }
+    }
+
+    if (!function_exists('loadMenuById')) {
+        function loadMenuById(int $id) {
+            \Laurel\Menu\MenuFacade::loadById($id);
+        }
+    }
+
