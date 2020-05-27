472cb1aa1313e2055d994be4ac5fee666a111acc
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-05-27 18:26:48 +0300

    Refactored package facade

--- src/MenuFacade.php
+++ src/MenuFacade.php
@@ -3,6 +3,7 @@
 namespace Laurel\Menu;
 
 use Laurel\Menu\App\Models\Menu;
+use Laurel\Menu\App\Models\MenuItem;
 
 /**
  * Main package class
@@ -13,26 +14,96 @@ use Laurel\Menu\App\Models\Menu;
 class MenuFacade
 {
     /**
-     * Load menu with items using id
+     * Loads menu with items using id
      *
      * @param int $id
      * @return Menu|null
      */
-    public static function loadById(int $id) : ?Menu
+    public static function loadMenuById(int $id) : ?Menu
     {
-        $menu = Menu::findById($id);
+        $menu = config('menu.menu_model')::findById($id);
         return $menu ? $menu->load('menuItems') : null;
     }
 
     /**
-     * Load menu with items using slug
+     * Loads menu with items using slug
      *
      * @param string $slug
      * @return Menu|null
      */
-    public static function loadBySlug(string $slug) : ?Menu
+    public static function loadMenuBySlug(string $slug) : ?Menu
     {
-        $menu = Menu::findBySlug($slug);
+        $menu = config('menu.menu_model')::findBySlug($slug);
         return $menu ? $menu->load('menuItems') : null;
+    }
+
+    /**
+     * Loads menu item using id
+     *
+     * @param int $id
+     * @return Menu|null
+     */
+    public static function loadMenuItemById(int $id) : ?MenuItem
+    {
+        return config('menu.menu_item_model')::findById($id);
+    }
+
+    /**
+     * Loads menu item using slug
+     *
+     * @param string $slug
+     * @return Menu|null
+     */
+    public static function loadMenuItemBySlug(string $slug) : ?MenuItem
+    {
+        return config('menu.menu_item_model')::findBySlug($slug);
+    }
+
+    /**
+     * Deletes menu using id
+     *
+     * @param int $id
+     * @return bool
+     */
+    public static function deleteMenuById(int $id) : bool
+    {
+        $menuItem = config('menu.menu_model')::findById($id);
+        return $menuItem ? $menuItem->delete() : true;
+    }
+
+    /**
+     * Deletes menu using slug
+     *
+     * @param string $slug
+     * @return bool
+     */
+    public static function deleteMenuBySlug(string $slug) : bool
+    {
+        $menuItem = config('menu.menu_model')::findBySlug($slug);
+        return $menuItem ? $menuItem->delete() : true;
+    }
+
+    /**
+     * Deletes menu item using id
+     *
+     * @param int $id
+     * @return bool
+     */
+    public static function deleteMenuItemById(int $id) : bool
+    {
+        $menuItem = config('menu.menu_item_model')::findById($id);
+        return $menuItem ? $menuItem->delete() : true;
+    }
+
+    /**
+     * Deletes menu item using slug
+     *
+     * @param string $slug
+     * @return bool
+     */
+    public static function deleteMenuItemBySlug(string $slug) : bool
+    {
+        $menuItem = config('menu.menu_item_model')::findBySlug($slug);
+        return $menuItem ? $menuItem->delete() : true;
     }
 }
